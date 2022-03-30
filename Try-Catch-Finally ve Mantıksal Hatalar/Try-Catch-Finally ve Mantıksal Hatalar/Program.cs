1cebe48c72ee1d988b76812a85f7774d1e1b09c9
elida ceylan <90249750+elidacy@users.noreply.github.com>
2022-03-30 23:50:35 +0300

    Add files via upload

---
+++ Try-Catch-Finally ve Mantıksal Hatalar/Try-Catch-Finally ve Mantıksal Hatalar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Try_Catch_Finally_ve_Mantıksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try //hata alması muhtemel kontrol altına alabileceğimiz kodları
+            {
+                Console.WriteLine("bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girmiş olduğunuz sayı:" + sayi);
+            }
+            catch(Exception ex) //hatayı yakaldığınızda ne yapmak istiyorsanız
+            {
+                Console.WriteLine("hata:" + ex.Message.ToString());
+            }
+
+            /* finally //hattadan bağımsız zorunlu değil
+             {
+                 Console.WriteLine("işlem tamamlandı.");
+             }*/
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("çok küçük yada çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally { Console.WriteLine("işlem başarıyla tamamlandı")};
+
+        }
+
+
+
+
+
+
+        }
+    }
+}
